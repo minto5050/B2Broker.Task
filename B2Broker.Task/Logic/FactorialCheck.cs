4b696a58f5a2ca09934c324c7271a945c0404ba1
Minto Issac <minto5050@gmail.com>
2023-02-11 22:26:30 +0400

    chore(*) : add factorial checks class

---
+++ B2Broker.Task/Logic/FactorialCheck.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2Broker.Task.Logic
+{
+	sealed class FactorialCheck
+	{
+		public int CheckFactorialsInFile(string filePath,int number)
+		{
+			List<int> factorialOf = new List<int>();
+			var numbersFromFile = ReadNumbersFromFile(filePath);
+			int result = CalculateReverseFactorial(number);
+			if (result != 0)
+			{
+				if (numbersFromFile.Contains(result))
+				{
+					return result;
+				}
+			}
+			return 0;
+		}
+		int[] ReadNumbersFromFile(string filePath)
+		{
+			string[] lines = File.ReadAllLines(filePath);
+			List<int> numbers = new List<int>();
+			foreach (string line in lines)
+			{
+				string[] parts = line.Split(' ');
+				foreach (string part in parts)
+				{
+					int n;
+					if (int.TryParse(part, out n))
+					{
+						numbers.Add(n);
+					}
+				}
+			}
+			return numbers.ToArray();
+		}
+
+		int CalculateReverseFactorial(int number)
+		{
+			int i = 1;
+			int factorial = 1;
+			while (factorial < number)
+			{
+				i++;
+				factorial *= i;
+			}
+			return factorial == number ? i : 0;
+		}
+	}
+}
